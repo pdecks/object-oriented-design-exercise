1f0528b8238275a1bc1d48cdaadc794a8ae3c9ab
Patricia Decker <patriciasquared@gmail.com>
2015-12-29 16:55:03 -0800

    fixed IDs (string formatting)

--- dropbox_classes.py
+++ dropbox_classes.py
@@ -34,13 +34,13 @@ def addClass(class_id, capacity, time):
 
     # check if course already exists
     if courses.get(class_id, 0) != 0:
-        return "Error adding class ID"
+        return "Error adding class %s" % class_id
   
     # add course
     new_course = Course(class_id, capacity, time)
     courses[class_id] = new_course
     
-    return "Successfully added class ID"
+    return "Successfully added class %s" % class_id
  
 
 def removeClass(class_id):
@@ -50,7 +50,7 @@ def removeClass(class_id):
   
     # check if course already exists
     if courses.get(class_id, 0) == 0:
-        return "Error removing class ID"
+        return "Error removing class %s" % class_id
 
     # grab course info
     course = courses[class_id]
@@ -70,7 +70,7 @@ def removeClass(class_id):
             # update item in students dictionary
             students[s_id] = student
 
-    return "Successfully removed class ID"
+    return "Successfully removed class %s" % class_id
     
   
 def infoClass(class_id):
@@ -86,14 +86,14 @@ def infoClass(class_id):
 
     # check if course already exists
     if courses.get(class_id, 0) == 0:
-        return "Class ID does not exist"
+        return "Class %s does not exist" % class_id
 
     course = courses.get(class_id)
     if not course.students:
-        return "Class ID is empty"
+        return "Class %s is empty" % class_id
     
     student_list = ",".join(sorted(course.students))
-    return_str = "Class ID has the following students: %s" % student_list
+    return_str = "Class %s has the following students: %s" % (class_id, student_list)
 
     return return_str
 
@@ -104,13 +104,13 @@ def addStudent(student_id, capacity, start, end):
     # Otherwise, return "Error adding student ID".
  
     if students.get(student_id, 0) != 0:
-        return "Error adding student ID"
+        return "Error adding student %s" % student_id
   
     # add student
     new_student = Student(student_id, capacity, start, end)
     students[student_id] = new_student
     
-    return "Successfully added student ID"
+    return "Successfully added student %s" % student_id
 
   
 def removeStudent(student_id):
@@ -119,7 +119,7 @@ def removeStudent(student_id):
     # Otherwise, return "Error removing student ID".
         # check if course already exists
     if students.get(student_id, 0) == 0:
-        return "Error removing student ID"
+        return "Error removing student %s" % student_id
 
     # grab student info
     student = students[student_id]
@@ -139,7 +139,7 @@ def removeStudent(student_id):
             # update item in students dictionary
             course[c_id] = course
 
-    return "Successfully removed student ID"
+    return "Successfully removed student %s" % student_id
 
   
 def infoStudent(student_id):
@@ -155,14 +155,14 @@ def infoStudent(student_id):
 
     # check if student already exists
     if students.get(student_id, 0) == 0:
-        return "Student ID does not exist"
+        return "Student %s does not exist" % student_id
 
     student = students.get(student_id)
     if not student.courses:
-        return "Student ID is not taking any classes"
+        return "Student %s is not taking any classes" % student_id
     
     course_list = ",".join(sorted(student.courses))
-    return_str = "Student ID is taking the following classes: %s" % course_list
+    return_str = "Student %s is taking the following classes: %s" % (student_id, course_list)
 
     return return_str
 
@@ -174,34 +174,35 @@ def enrollStudent(student_id, class_id):
     # in the class after the student enrolls. 
     # Otherwise, return "Enrollment of student STUDENTID in class CLASSID failed".
 
+    fail_str = "Enrollment of student %s in class %s failed" % (student_id, class_id)
     # check student exists and class exists
     if students.get(student_id, 0) == 0 or courses.get(class_id, 0) == 0:
-        return "Enrollment of student STUDENTID in class CLASSID failed"
+        return fail_str
 
     # check student not already enrolled in class
     student = students.get(student_id)
     if student.courses and class_id in student.courses:
-        return "Enrollment of student STUDENTID in class CLASSID failed"
+        return fail_str
 
     # check student has capacity to enroll
     if len(student.courses) == student.capacity:
-        return "Enrollment of student STUDENTID in class CLASSID failed"
+        return fail_str
 
     # check class has capacity for student
     course = courses.get(class_id)
     if len(course.students) == course.capacity:
-        return "Enrollment of student STUDENTID in class CLASSID failed"
+        return fail_str
 
     # check class time inside student's available time interval
     if course.time > student.end or course.time < student.start:
-        return "Enrollment of student STUDENTID in class CLASSID failed"
+        return fail_str
 
     # check that student is not already taking a class at that time
     # get all course times for courses taken by student
     if student.courses:
         for c_id in student.courses:
             if courses[c_id].time == course.time:
-                return "Enrollment of student STUDENTID in class CLASSID failed"
+                return fail_str
 
     # add student to course
     course.students.append(student_id)
@@ -212,8 +213,8 @@ def enrollStudent(student_id, class_id):
     students[student_id] = student
 
     course_spots = course.capacity - len(course.students)
-    return_str = "Number of free spots left in class CLASSID: " % course_spots
-    return
+    return_str = "Number of free spots left in class %s: " % (class_id, course_spots)
+    return return_str
 
 
 def unenrollStudent(student_id, class_id):
@@ -223,14 +224,15 @@ def unenrollStudent(student_id, class_id):
     # after the student unenrolls. Otherwise, return "Unenrollment 
     # of student STUDENTID in class CLASSID failed".
 
+    fail_str = "Unenrollment of student %s in class %s failed" % (student_id, class_id)
     # check student exists and class exists
     if students.get(student_id, 0) == 0 or courses.get(course_id, 0) == 0:
-        return "Unenrollment of student STUDENTID in class CLASSID failed"
+        return fail_str
 
     # check student already enrolled in class
     student = students.get(student_id)
     if class_id not in student.courses:
-        return "Unenrollment of student STUDENTID in class CLASSID failed"
+        return fail_str
 
     # remove student from course's students list
     course = courses.get(class_id)
@@ -245,7 +247,7 @@ def unenrollStudent(student_id, class_id):
     students[student_id] = student
 
     course_spots = course.capacity - len(course.students)
-    return_str = "Number of free spots left in class CLASSID: " % course_spots
+    return_str = "Number of free spots left in class %s: " % (class_id, course_spots)
     return return_str
 
 
